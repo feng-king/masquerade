253a59bd85b9d555a7975de717a760f06fe5e996
feng-king <82305418+feng-king@users.noreply.github.com>
2023-04-02 11:11:08 +0800

    Update client.rs
    update CongestionControlAlgorithm

--- src/client.rs
+++ src/client.rs
@@ -142,7 +142,7 @@ impl Client {
         
         config.verify_peer(!skip_verify);
         config.set_application_protos(quiche::h3::APPLICATION_PROTOCOL).unwrap();
-        
+        config.set_cc_algorithm(quiche::CongestionControlAlgorithm::Reno);
         config.set_max_idle_timeout(1000);
         config.set_max_recv_udp_payload_size(MAX_DATAGRAM_SIZE);
         config.set_max_send_udp_payload_size(MAX_DATAGRAM_SIZE);
